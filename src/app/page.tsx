6f72df620f53edc6054dcc5ec1c4f2c5b0c48fd6
Yaroslav Matvieiev <yaromatv@gmail.com>
2024-09-11 14:29:16 +0200

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import StatusLabel, { Status } from './components/status-label';
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl">Home page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Non Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+    </main>
+  );
+}
